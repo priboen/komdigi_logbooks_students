63a6e77ca1878e36cbc51e214014b4a04f3084df
Adri Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2025-01-07 00:32:37 +0700

    add folder core

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,5 @@
+class Variables {
+  static const String appName = 'SIMKP Komdigi Admin';
+  // static const String baseUrl = 'http://10.0.2.2:8000';
+  static const String baseUrl = 'http://172.16.11.161:8000';
+}
